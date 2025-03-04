0a97b7042e209ef767f88efbaad4aa8e8451e758
tnx2401 <pttnghia24@gmail.com>
2025-03-04 15:48:31 +0700

    update env

--- project/lib/firebaseAdmin.js
+++ project/lib/firebaseAdmin.js
@@ -2,7 +2,7 @@ const admin = require("firebase-admin");
 
 // Check if the app has already been initialized
 if (!admin.apps.length) {
-  const serviceAccount = require("./realestate-project-28361-firebase-adminsdk-vrtw3-55eb9559d6.json");
+  const serviceAccount = require(process.env.FIREBASE_CREDENTIALS);
 
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
